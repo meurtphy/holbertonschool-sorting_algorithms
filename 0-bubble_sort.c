e67bb1a8a5bc98b024986fbdbee74abe66e1a864
meurtphy <erwanmpn5@gmail.com>
2024-12-10 14:39:22 +0100

    d

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,10 +1,15 @@
 #include "sort.h"
 #include <stdio.h>
 
+/**
+ * bubble_sort - Trie un tableau d'entiers en ordre croissant
+ *               avec l'algorithme Bubble Sort.
+ * @array: Le tableau à trier
+ * @size: Taille du tableau
+ */
 void bubble_sort(int *array, size_t size)
-
 {
-
+size_t i;
 int temp;
 int swapped;
 
@@ -13,15 +18,16 @@ return;
 
 do {
 swapped = 0;
-for (size_t i = 0; i < size - 1; i++)
+for (i = 0; i < size - 1; i++)
 {
-
 if (array[i] > array[i + 1])
 {
 
 temp = array[i];
 array[i] = array[i + 1];
 array[i + 1] = temp;
+
+
 swapped = 1;
 
 
@@ -31,5 +37,4 @@ print_array(array, size);
 size--;
 } while (swapped);
 }
-
 
