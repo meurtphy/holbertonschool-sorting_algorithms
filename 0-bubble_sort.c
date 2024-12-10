4992d4ea30af46f02969860dd588222a67be9614
meurtphy <erwanmpn5@gmail.com>
2024-12-10 14:53:14 +0100

    d

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -22,14 +22,11 @@ for (i = 0; i < size - 1; i++)
 {
 if (array[i] > array[i + 1])
 {
-
 temp = array[i];
 array[i] = array[i + 1];
 array[i + 1] = temp;
 
-
 swapped = 1;
-
 
 print_array(array, size);
 }
