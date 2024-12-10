1968a99af3f6a8d3160549010c76a0097177cfc9
meurtphy <erwanmpn5@gmail.com>
2024-12-10 14:28:59 +0100

    d

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,26 +11,25 @@ int swapped;
 if (size < 2)
 return;
 
-do
-
-{
+do {
 swapped = 0;
-
 for (size_t i = 0; i < size - 1; i++)
 {
 
 if (array[i] > array[i + 1])
 {
+
 temp = array[i];
 array[i] = array[i + 1];
 array[i + 1] = temp;
 swapped = 1;
+
+
+print_array(array, size);
 }
 }
 size--;
+} while (swapped);
 }
 
-while (swapped);
-
-}
 
