590a064147a56ca09ab4edba662ce40b217edbac
meurtphy <erwanmpn5@gmail.com>
2024-12-10 17:38:32 +0100

    d

--- sort.h
+++ sort.h
@@ -3,22 +3,24 @@
 
 #include <stddef.h>
 
-/**
- * struct listint_s - Nœud d'une liste doublement chaînée
- *
- * @n: Entier stocké dans le nœud
- * @prev: Pointeur vers l'élément précédent de la liste
- * @next: Pointeur vers l'élément suivant de la liste
- */
+
 typedef struct listint_s
+
 {
+
 const int n;
 struct listint_s *prev;
 struct listint_s *next;
 } listint_t;
 
 
+void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
+
+void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif
